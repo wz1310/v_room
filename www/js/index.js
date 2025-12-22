d57ac397588e9e25f361fdd0e7b1b81926b13868
wz1310 <a002041zano@gmail.com>
2025-12-22 15:11:12 +0700

    Update: 22-12-2025_15:11:12

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
   const BASE_URL = "https://m3h048qq-3000.asse.devtunnels.ms";
+  //dddddddddd
   const loginForm = document.getElementById("loginForm");
   const passwordInput = loginForm.querySelector('input[type="password"]');
 
